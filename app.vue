b1e3a91c26ad1b797f332a75373c167bb25546f5
abarraghi <abarraghi@gmail.com>
2024-02-25 18:47:31 -0500

    Multipage functionality to app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
+
+import { NuxtPage } from '#build/components';
 <template>
   <div>
-    <h1>Main Page</h1>
-
-    <a href="" class="btn btn-primary">Button of Bootstrap</a>
+    <NuxtPage />
   </div>
 </template>
